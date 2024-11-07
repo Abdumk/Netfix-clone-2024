fc6748ba6074d439d1ddadbb2a33e64e570e24c3
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-11-07 17:37:25 +0300

    header & footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1> NETFLIX PRACTICE</h1>
+     <Home />
     </div>
   );
 }
