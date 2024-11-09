4d54b7aac37ca44f41988fd64112dc91f948b32d
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-11-09 17:23:32 +0300

    banner-part

---
+++ netflix-clone/src/Components/Rows/Row/Row.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../Row/Row.css'
+function Row() {
+  return (
+    <div>Row</div>
+  )
+}
+
+export default Row
---
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Row from '../Row/Row'
+function RowList() {
+  return (
+    <>
+    <Row />
+    </>
+  )
+}
+
+export default RowList
