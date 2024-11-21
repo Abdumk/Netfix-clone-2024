4365e167884318338cdcc8c203753e19c146f671
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-11-21 23:52:51 +0300

    add different dep

---
+++ netflix-clone/netflix-clone/src/App.js
@@ -0,0 +1,13 @@
+// API KEY = 43a929250e8c2b32187ff04dd4256ee1
+import './App.css';
+import Home from './Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/netflix-clone/src/Components/Footer/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import'./Footer.css'
+function Footer() {
+  return (
+    <div className='footer_outer_container'>
+    <div className='footer_inner_container'>
+      <div className='footer_icons'>
+        <FacebookOutlinedIcon />
+        <InstagramIcon />
+        <YouTubeIcon />
+      </div>
+      <div className='footer_data'>
+        <div>
+          <ul>
+            <li><a href="#">Audio Description</a></li>
+            <li><a href="#">Investor Relations</a></li>
+            <li><a href="#">Legal Notice</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="#">Help Center</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Cookie Preferences</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="#">Gift Cards</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Corporate Information</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="#">Media Center</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className='service_code'>
+        <p>
+          Service Code
+        </p>
+      </div>
+      <div className='copy-write'>
+        &copy; 1997-2024 Netflix, Inc.
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Footer
